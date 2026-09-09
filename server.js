958742751854b722b4fc0081a297c44303ddc959
Dhanya P <pdhanya098@gmail.com>
2026-09-09 13:30:42 +0530

    day 11: express server with API routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my backend!');
+});
+app.get('/api/students', (req, res) => {
+  const students = [
+    { name: "Dhanya", course: "MERN" },
+    { name: "Anu", course: "Python" }
+  ];
+  res.json(students);
+});
+
+app.get('/api/students/:name', (req, res) => {
+  res.send(`Looking for student: ${req.params.name}`);
+});
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
